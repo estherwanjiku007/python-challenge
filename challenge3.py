1e19e57fe51ec7cf4dcb5766b397acc477ab5068
estherwanjiku007 <estherwanjiqu@gmail.com>
2024-03-06 16:15:30 +0300

    ain

--- challenge3.py
+++ challenge3.py
@@ -2,7 +2,7 @@ all_list2=["Esther","Beatrice","BEN","Wanjiku","Mugo"]
 def gives_string(an_int,all_list):  
    new_list=list()  
    for a in all_list:
-             
+       
          if len(a)==an_int :
           new_list.append(a.lower())     
           
